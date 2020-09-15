4117566bf6737e9e3e51dc3e648f2a4e50b640a2
Artem Orlov <universalism123@gmail.com>
2020-09-16 01:21:55 +0200

    commit

---
+++ checkFunc.js
@@ -0,0 +1,20 @@
+function getPasswordFromUser(success, fail) {
+    let password = prompt("Password?", '');
+    if (password == "rockstar") success();
+    else fail();
+};
+  
+let user = {
+    name: 'Andrew',
+  
+    loginSuccess() {
+      alert(`${this.name} logged in`);
+    },
+  
+    loginFail() {
+      alert(`${this.name} failed to log in`);
+    },
+  
+};
+  
+  getPasswordFromUser(user.loginSuccess.bind(user), user.loginFail.bind(user));
